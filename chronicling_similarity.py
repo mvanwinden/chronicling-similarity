659ee2e94803d233960ca2518df36b3de4f06121
Max <107750786+mvanwinden@users.noreply.github.com>
2023-02-25 22:00:30 +0100

    Update chronicling_similarity.py

--- chronicling_similarity.py
+++ chronicling_similarity.py
@@ -1,111 +1,89 @@
 import pandas as pd
 import numpy as np
-import itertools
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-parsedChronicles = 'parsed_rotterdam_chronicles.ndjson'
-
-def shingle(text: str, k: int):
-
-    shingles = []
-
-    for i in range(len(text) - k + 1):
-        
-        shingles.append(text[i:i+k])
-
-    return set(shingles)
-
-df = pd.read_json(parsedChronicles, lines = True)
-
-df['date'] = df['date'].str.get(0).replace('-.*', '', regex= True)
-
-df['date'] = df['date'].astype(str)
-
-df['call_nr'] = df['call_nr'].str.replace('_.*', '', regex= True)
-
-dfDateContents = df.pivot_table(values = 'text', index = 'date', columns = 'call_nr', aggfunc = 'sum', fill_value = np.nan)
-dfDateContentsBack = df.pivot_table(values = 'text', index = 'date', columns = 'call_nr', aggfunc = 'sum', fill_value = 0)
-
-dfDateContentsBack[dfDateContentsBack != 0] = 1
-dfDateContentsBack = dfDateContentsBack.astype(int)
-dfDateContentsBack = dfDateContentsBack[(dfDateContentsBack.T != 0).any()]
-
-
-df1 = dfDateContentsBack.iloc[:len(dfDateContentsBack.index)//3, :]
-df2 = dfDateContentsBack.iloc[len(dfDateContentsBack.index)//3: 2*(len(dfDateContentsBack.index)//3), :]
-df3 = dfDateContentsBack.iloc[2*(len(dfDateContentsBack.index)//3):, :]
-
-fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(10, 20), gridspec_kw={'width_ratios': [1,1,1], 'wspace': 0.3})
-sns.heatmap(df1, cmap = 'Reds', vmin = 0.5, vmax = 1.5, ax=axs[0], linewidths=0.5, linecolor='black', cbar=False)
-axs[0].set_title('')
-axs[0].set_xlabel('')
-axs[0].set_ylabel('Years')
-
-sns.heatmap(df2, cmap = 'Reds', vmin = 0.5, vmax = 1.5, ax=axs[1], linewidths=0.5, linecolor='black', cbar=False)
-axs[1].set_title('Years present in chronicle')
-axs[1].set_xlabel('Chronicles')
-axs[1].set_ylabel(' ')
-
-sns.heatmap(df3, cmap = 'Reds', vmin = 0.5, vmax = 1.5, ax=axs[2], linewidths=0.5, linecolor='black', cbar=False)
-axs[2].set_title('')
-axs[2].set_xlabel('')
-axs[2].set_ylabel(' ')
-
-plt.show()
-
-for column in dfDateContents.columns:
-
-    if dfDateContents[column].dtype == "object":
-
-        dfDateContents[column] = dfDateContents[column].str.replace('[^\w]','', regex = True)
-
-        dfDateContents[column] = dfDateContents[column].str.lower()
-        
-        dfDateContents[column] = dfDateContents[column].apply(lambda x: shingle(str(x), 3) if pd.notna(x) else x)
+def importCorpus(corpus):
     
-columnPairs = list(itertools.combinations(dfDateContents.columns, 2))
-
-dfJaccardSimilarities = pd.DataFrame(index = dfDateContents.index, columns = columnPairs)
-
-dfJaccardSimilarities.rename(columns = {col: ", ".join(col) for col in dfJaccardSimilarities.columns}, inplace = True)
-
-jSimList = []
-
-for n in range(len(columnPairs)):
-
-    for date in dfDateContents.index:
-
-        if pd.notna(dfDateContents.at[date, columnPairs[n][0]]) and pd.notna(dfDateContents.at[date, columnPairs[n][1]]):
-
-            jSim = []
-
-            fileOne = dfDateContents.at[date, columnPairs[n][0]]
-
-            fileTwo = dfDateContents.at[date, columnPairs[n][1]]
-
-            jaccardSimilarity = len(fileOne.intersection(fileTwo)) / len(fileOne.union(fileTwo))
-
-            jSim = [columnPairs[n], date, jaccardSimilarity]
-
-            jSimList.append(jSim)
-
-for x, y, value in jSimList:
-
-    x = ', '.join(x)
+    corpusDataFrame = pd.read_json(corpus, lines=True)
+    corpusDataFrame['date'] = corpusDataFrame['date'].str[0].astype(str)
+    contents = corpusDataFrame.pivot_table(values='text', index='date', columns='call_nr', aggfunc='sum', fill_value=np.nan)
     
-    dfJaccardSimilarities.loc[y, x] = value
+    return contents
 
-dfJaccardSimilarities.dropna(how = 'all', inplace = True)
+def prepareCorpus(contents):
 
-dfJaccardSimilarities = dfJaccardSimilarities.apply(pd.to_numeric, errors = 'coerce')
+    for column in contents.columns:
 
-sns.set(rc={'figure.figsize': (10, 30)})
+        if contents[column].dtype == 'object':
 
-sns.heatmap(dfJaccardSimilarities, cmap = sns.cm.rocket_r, vmin = 0, vmax = 1, linewidths=0.0, linecolor='black', annot = False)
+            contents[column] = contents[column].str.replace('[^\w]','', regex = True)
+            contents[column] = contents[column].str.lower()
+            contents[column] = contents[column].apply(lambda x: set([x[i:i+3] for i in range(len(x)-2)]) if pd.notna(x) else x)
 
-plt.title('Jaccard Similarities Heat Map')
-plt.xlabel('Chronicle pairs')
-plt.ylabel('Year')
+    return contents
 
-plt.show()
+def jaccardSimilarity(contentsClean):
+
+    chroniclePairs = list(itertools.combinations(contentsClean.columns, 2))
+
+    jaccardSimilaritiesDataframe = pd.DataFrame(index = contentsClean.index, columns = chroniclePairs)
+
+    jaccardSimilaritiesDataframe.rename(columns = {col: ", ".join(col) for col in jaccardSimilaritiesDataframe.columns}, inplace = True)
+
+
+    jaccardSimilarityList = []
+
+    for n in range(len(chroniclePairs)):
+
+        for date in contentsClean.index:
+
+            if pd.notna(contentsClean.at[date, chroniclePairs[n][0]]) and pd.notna(contentsClean.at[date, chroniclePairs[n][1]]):
+
+                jSim = []
+
+                fileOne = contentsClean.at[date, chroniclePairs[n][0]]
+
+                fileTwo = contentsClean.at[date, chroniclePairs[n][1]]
+
+                jaccardSimilarity = len(fileOne.intersection(fileTwo)) / len(fileOne.union(fileTwo))
+
+                jSim = [chroniclePairs[n], date, jaccardSimilarity]
+
+                jaccardSimilarityList.append(jSim)
+
+    for x, y, value in jaccardSimilarityList:
+
+        x = ', '.join(x)
+        
+        jaccardSimilaritiesDataframe.loc[y, x] = value
+
+    jaccardSimilaritiesDataframe.dropna(how = 'all', inplace = True)
+
+    jaccardSimilaritiesDataframe = jaccardSimilaritiesDataframe.apply(pd.to_numeric, errors = 'coerce')
+
+    return jaccardSimilaritiesDataframe
+
+def jaccardSimilarityHeatmap(jaccard_similarities):
+
+    sns.set(rc={'figure.figsize': (10, 30)})
+    sns.heatmap(jaccard_similarities, cmap=sns.cm.rocket_r, vmin=0, vmax=1, linewidths=0.0, linecolor='black', annot=False)
+    plt.title('Jaccard Similarities Heat Map')
+    plt.xlabel('Chronicle pairs')
+    plt.ylabel('Year')
+    return plt.gcf()
+
+def main():
+
+    corpus = 'test_corpus.ndjson'
+    contents = importCorpus(corpus)
+    contentsClean = prepareCorpus(contents)
+    jaccardSimilarities = jaccardSimilarity(contentsClean)
+    heatmap = jaccardSimilarityHeatmap(jaccardSimilarities)
+    plt.show(heatmap)
+    
+if __name__ == '__main__':
+
+    main()
+    
+ 
